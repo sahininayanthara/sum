0a6ad61a6fe3f5b0d1b61c6d24a615a214cfaa67
sahininayanthara <sahininayanthara95@gmail.com>
2019-02-21 22:35:25 +0530

    first commit

---
+++ src/sum/Sum.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Sum {
+
+    public static void main(String[] args) {
+        Scanner b =new Scanner(System.in);
+        double fnum,snum,answer;
+        
+         System.out.println("enter the first number");
+        fnum=b.nextDouble();
+       
+         System.out.println("enter the second number");
+        snum=b.nextDouble();
+        
+        answer =fnum+snum;
+         System.out.println(answer);
+        
+        
+        
+        
+        // TODO code application logic here
+    }
+    
+}
